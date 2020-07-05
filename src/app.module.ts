1e305ad5260f30e170636248fe66a77b423c3aed
Mattias Kjellsson <u.m.kjellsson@gmail.com>
2020-07-05 21:11:38 +0700

    Add postman tests, update readme, add environment variables for database.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,22 +7,25 @@ import { DatabaseModule } from './database/database.module';
 import { RestaurantModule } from './restaurant/restaurant.module';
 import { OrdersModule } from './orders/orders.module';
 import { UserModule } from './admin/user/user.module';
-
+// require('dotenv').config();
+import * as dotenv from 'dotenv';
+dotenv.config();
 @Module({
   imports: [
+    // TODO:: Move this stuff to environment...
     TypeOrmModule.forRoot(
       {
         "name": "default",
         "type": "postgres",
-        "host": "localhost", 
-        "username": "dbUser", 
-        "password": "toor",
-        "database": "yum",
-        "port": 5431,
-        "ssl": false,
-        "synchronize": true,
-        "dropSchema": true,
-        "migrationsRun": true,
+        "host": process.env.DB_HOST,
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASS,
+        "database": process.env.DB_DATABASE,
+        "port": +process.env.DB_PORT,
+        "ssl": (process.env.DB_SSL === 'true'),
+        "synchronize": (process.env.DB_SYNCHRONIZE === 'true'),
+        "dropSchema": (process.env.DB_DROP_SCHEMA === 'true'),
+        "migrationsRun": (process.env.DB_MIGRATIONS_RUN === 'true'),
         "logging": "all",
         "entities": [
           "dist/entities/*.entity.js"
